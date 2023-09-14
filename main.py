bf6638448a195bff840d54f56d3e354e4472dbf9
dan <danya.belyaev@gmail.com>
2023-09-14 22:34:28 +0300

    0.0.1

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+app.debug = True
+
+history = []
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    expression = request.form['display']
+
+    result = eval(expression)
+
+    history.append({"expression": str(expression), "result": str(result)})
+
+    return render_template('index.html', result=result, history=history)
+
+
+if __name__ == '__main__':
+    app.run()
+
