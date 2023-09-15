26855615822f5dfc853ad61864538a5185c677ad
AmEl <amirelkanov0504@gmail.com>
2023-09-15 20:53:22 +0300

    First demo

---
+++ static/restore.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const historyItems = document.querySelectorAll(".history-item");
+
+
+    historyItems.forEach(function (item) {
+        item.addEventListener("click", function () {
+            const result = item.getAttribute("data-result");
+            document.getElementsByName("display").value = result;
+            alert(document.getElementsByName("display").value);
+            fetch('/calculate', {
+                method: 'POST',
+            }).then(data => {
+                window.location.reload();
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        });
+    });
+});
