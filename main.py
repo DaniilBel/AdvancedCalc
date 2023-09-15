c9015a389e9de4154c9be7e4870856f22a48f6d9
verbinna22 <mylogininya@yandex.ru>
2023-09-15 21:09:35 +0300

    add all checks and raise exceptions

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, render_template
+import re
 
 app = Flask(__name__)
 app.debug = True
 
 history = []
+
+
+class ParseError(ValueError):
+    pass
 
 
 @app.route('/')
@@ -14,8 +19,16 @@ def index():
 @app.route('/calculate', methods=['POST'])
 def calculate():
     expression = request.form['display']
-
-    result = eval(expression)
+    # потом нормально обработать исключения с выводом сообщения в окне
+    if re.fullmatch(r"\A[()0-9+*/^%-]*\Z", expression) is None:
+        raise ParseError("There is unsupported symbols in request!")
+    if len(re.findall(r"(?:[+*/^%-]--)|(?:\+\+\+)|(?://)|(?:\*\*)", expression)) != 0:
+        raise ParseError("There is unsupported combination of symbols in request!")
+    try:
+        # если будет поддержка ^ - заменить здесь на **
+        result = eval(expression)
+    except ZeroDivisionError:
+        raise ZeroDivisionError("Division by zero found!")
 
     history.append({"expression": str(expression), "result": str(result)})
 
@@ -24,4 +37,3 @@ def calculate():
 
 if __name__ == '__main__':
     app.run()
-
