25fa467d692cf313f8c6a14db1b9d0f41bda6acb
verbinna22 <mylogininya@yandex.ru>
2023-09-15 22:54:21 +0300

    replace exceptions

--- main.py
+++ main.py
@@ -21,15 +21,22 @@ def calculate():
     expression = request.form['display']
     # потом нормально обработать исключения с выводом сообщения в окне
     if re.fullmatch(r"\A[()0-9+*/^%-]*\Z", expression) is None:
-        raise ParseError("There is unsupported symbols in request!")
+        return render_template(
+            'index.html',
+            result="There is unsupported symbols in request!",
+            history=history
+        )
     if len(re.findall(r"(?:[+*/^%-]--)|(?:\+\+\+)|(?://)|(?:\*\*)", expression)) != 0:
-        raise ParseError("There is unsupported combination of symbols in request!")
+        return render_template(
+            'index.html',
+            result="There is unsupported combination of symbols in request!",
+            history=history
+            )
     try:
         # если будет поддержка ^ - заменить здесь на **
         result = eval(expression)
     except ZeroDivisionError:
-        raise ZeroDivisionError("Division by zero found!")
-
+        return render_template('index.html', result="Division by zero found!", history=history)
     history.append({"expression": str(expression), "result": str(result)})
 
     return render_template('index.html', result=result, history=history)
