059f90ca18235cc9a81b3ce42bd24308c7c6ded9
AmEl <amirelkanov0504@gmail.com>
2023-09-15 23:30:40 +0300

    Connected DB to main.py & index.html

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 from flask import Flask, request, render_template
 import re
+from database.model.connector import create_connection
 
 app = Flask(__name__)
 app.debug = True
-
-history = []
 
 
 class ParseError(ValueError):
     pass
 
 
-def inform_user(msg: str):
-    app.logger.info("Calculation problem: %s", msg)
-    print(msg)  # add code to inform user
-
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM calc_history")
+    return render_template('index.html', history=cursor.fetchall())
 
 
 @app.route('/calculate', methods=['POST'])
@@ -26,29 +22,24 @@ def calculate():
     expression = request.form['display']
     # потом нормально обработать исключения с выводом сообщения в окне
     if re.fullmatch(r"\A[()0-9+*/^%-]*\Z", expression) is None:
-        inform_user("There is unsupported symbols in request!")
-        return render_template(
-            'index.html',
-            result="",
-            history=history
-        )
+        raise ParseError("There is unsupported symbols in request!")
     if len(re.findall(r"(?:[+*/^%-]--)|(?:\+\+\+)|(?://)|(?:\*\*)", expression)) != 0:
-        inform_user("There is unsupported combination of symbols in request!")
-        return render_template(
-            'index.html',
-            result="",
-            history=history
-        )
+        raise ParseError("There is unsupported combination of symbols in request!")
+
     try:
         # если будет поддержка ^ - заменить здесь на **
         result = eval(expression)
+        cursor = connection.cursor()
+        sql = "INSERT INTO calc_history (line, answer) VALUES (%s, %s)"
+        val = (expression, result)
+        cursor.execute(sql, val)
     except ZeroDivisionError:
-        inform_user("Division by zero found!")
-        return render_template('index.html', result="", history=history)
-    history.append({"expression": str(expression), "result": str(result)})
+        raise ZeroDivisionError("Division by zero found!")
 
-    return render_template('index.html', result=result, history=history)
+    cursor.execute("SELECT * FROM calc_history")
+    return render_template('index.html', result=result, history=cursor.fetchall())
 
 
 if __name__ == '__main__':
+    connection = create_connection()
     app.run()
