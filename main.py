ea36e385da525b0bee4ba5d595df84a55600b043
verbinna22 <mylogininya@yandex.ru>
2023-09-15 23:04:33 +0300

    add logging

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ class ParseError(ValueError):
     pass
 
 
+def inform_user(msg: str):
+    app.logger.info("Calculation problem: %s", msg)
+    print(msg)  # add code to inform user
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -21,22 +26,25 @@ def calculate():
     expression = request.form['display']
     # потом нормально обработать исключения с выводом сообщения в окне
     if re.fullmatch(r"\A[()0-9+*/^%-]*\Z", expression) is None:
+        inform_user("There is unsupported symbols in request!")
         return render_template(
             'index.html',
-            result="There is unsupported symbols in request!",
+            result="",
             history=history
         )
     if len(re.findall(r"(?:[+*/^%-]--)|(?:\+\+\+)|(?://)|(?:\*\*)", expression)) != 0:
+        inform_user("There is unsupported combination of symbols in request!")
         return render_template(
             'index.html',
-            result="There is unsupported combination of symbols in request!",
+            result="",
             history=history
-            )
+        )
     try:
         # если будет поддержка ^ - заменить здесь на **
         result = eval(expression)
     except ZeroDivisionError:
-        return render_template('index.html', result="Division by zero found!", history=history)
+        inform_user("Division by zero found!")
+        return render_template('index.html', result="", history=history)
     history.append({"expression": str(expression), "result": str(result)})
 
     return render_template('index.html', result=result, history=history)
