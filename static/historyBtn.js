6c39c129a203a4058691d5cb24d9782542d7b043
dan <danya.belyaev@gmail.com>
2023-09-17 17:37:49 +0300

    add rotation button

--- static/historyBtn.js
+++ static/historyBtn.js
@@ -1,13 +1,28 @@
 const historyBtn = document.getElementById('open-history');
 const panel = document.getElementById('history');
+const image = historyBtn.querySelector('img');
 
 const panelState = localStorage.getItem('panelState');
+const imageRotation = localStorage.getItem('imageRotation');
+
+if (imageRotation) {
+    image.style.transform = `rotate(${imageRotation}deg)`;
+}
 
 if (panelState === 'opened') {
     panel.classList.add('opened');
 }
 
-historyBtn.addEventListener('click', function() {
+historyBtn.addEventListener('click', () => {
+    const currentRotation = getRotationValue(image);
+    const newRotation = currentRotation + 180;
+
+    image.style.transform = `rotate(${newRotation}deg)`;
+
+    localStorage.setItem('imageRotation', newRotation);
+})
+
+historyBtn.addEventListener('click', () => {
     panel.classList.toggle('opened');
 
     if (panel.classList.contains('opened')) {
@@ -16,3 +31,16 @@ historyBtn.addEventListener('click', function() {
         localStorage.setItem('panelState', 'closed');
     }
 });
+
+// 
+function getRotationValue(element) {
+    const style = window.getComputedStyle(element);
+    const matrix = style.getPropertyValue('transform');
+    if (matrix && matrix !== 'none') {
+        const values = matrix.split('(')[1].split(')')[0].split(',');
+        const a = values[0];
+        const b = values[1];
+        return Math.round(Math.atan2(b, a) * (180 / Math.PI));
+    }
+    return 0;
+}
