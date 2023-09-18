9f868a48d961d6e42151ada229b6a1d12a7666bc
В. Н. А. СП МКН <st107903@student.spbu.ru>
2023-09-18 19:40:25 +0300

    fixed test

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ app.debug = True
 
 
 def expression_check(expression) -> (bool, str):
-    if re.fullmatch(r"\A[\t ()0-9+*/^%.-]*\Z", expression) is None:
+    if re.fullmatch(r"\A[\s()0-9+*/^%.-]*\Z", expression) is None:
         return False, "There are unsupported symbols in request!"
-    if len(re.findall(r"(?:[+*/^%-]--)|(?:\+\+\+)|(?://)|(?:\*\*)", expression)) != 0:
+    if len(
+            re.findall(
+                r"(?:[+*/^%-]\s*-\s*-)|(?:\+\s*\+\s*\+)|(?:/\s*/)|(?:\*\s*\*)|(?:\.\s*\.)",
+                expression,
+            )
+    ) != 0:
         return False, "There are unsupported combinations of symbols in request!"
     return True,
 
