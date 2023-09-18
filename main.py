25a94e903dcc2b8e8bafc73b63108d84858147b9
Tsagolov George Zaurbekovich <gtsagolov8@gmail.com>
2023-09-18 04:01:09 +0300

    created some tests, chenged the function "expression_check"

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ app.debug = True
 
 
 def expression_check(expression) -> (bool, str):
-    if re.fullmatch(r"\A[()0-9+*/^%.-]*\Z", expression) is None:
-        return False, "There is unsupported symbols in request!"
+    if re.fullmatch(r"\A[\t ()0-9+*/^%.-]*\Z", expression) is None:
+        return False, "There are unsupported symbols in request!"
     if len(re.findall(r"(?:[+*/^%-]--)|(?:\+\+\+)|(?://)|(?:\*\*)", expression)) != 0:
-        return False, "There is unsupported combination of symbols in request!"
+        return False, "There are unsupported combination of symbols in request!"
     return True,
 
 
